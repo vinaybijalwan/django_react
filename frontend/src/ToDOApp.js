c699dfe4ba87696d5b959a855def16c27e1cdfa6
vinay <bijalwan.vinay@gmail.com>
2024-01-20 16:59:43 +0530

    react api call

---
+++ frontend/src/ToDOApp.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect }  from 'react'
+
+export const ToDOApp = () => {
+
+    const[mydata, Setmydata] = useState("")
+
+    useEffect(() => {
+        //Define  URL
+        const url = "http://127.0.0.1:8000/todo/"
+        //Create the asynchronous function
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                console.log(json);
+                
+                Setmydata(json);   // update value of  state 
+                } catch (error) {
+                console.log("error", error);
+                }
+        }; fetchData();
+    }, [])
+
+  return (
+    <>
+        <div>
+            {/* {
+                mydata.map((curElem) => {
+                    return (
+                        <h2>{curElem.date}</h2>
+                    )
+                }
+
+                )  
+            } */}
+
+        </div>
+    </>
+  )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Todo } from './Todo';
+import { ToDOApp } from './ToDOApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Todo />
+    <ToDOApp />
   </React.StrictMode>
 );
 
